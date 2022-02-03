c9b0c447ef40082ef4b2c721bddbb54b4d4d1e56
povadok <82115304+povadok@users.noreply.github.com>
2022-02-03 22:10:13 +0400

    added main page

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,118 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.getElementById('btn_1').addEventListener('click', function () {
+        inputWindow.value += '1';
+})
+
+document.getElementById('btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+document.getElementById('btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+document.getElementById('btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+
+document.getElementById('btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+
+document.getElementById('btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+document.getElementById('btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+
+document.getElementById('btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+document.getElementById('btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+document.getElementById('btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = "sum"
+    inputWindow.value = '';
+
+})
+
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = "def"
+    inputWindow.value = '';
+
+})
+
+document.getElementById('btn_multipli').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = "multipli"
+    inputWindow.value = '';
+
+})
+
+document.getElementById('btn_sqrt').addEventListener('click', function () {
+    // lastOperand = parseInt(inputWindow.value);
+    // operation = "sqrt"
+    const result = Math.sqrt(parseInt(inputWindow.value));
+    lastOperand = 0;
+    operation = null;
+    inputWindow.value = result;
+
+
+})
+
+document.getElementById('btn_div').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = "div"
+    inputWindow.value = '';
+
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if(operation === "sum"){
+        const result = lastOperand +  parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+    if(operation === "def"){
+        const result = lastOperand -  parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+    if(operation === "multipli"){
+        const result = lastOperand *  parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+    if(operation === "div"){
+        const result = lastOperand /  parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+    // if(operation === "sqrt"){
+    //     const result =Math.sqrt(lastOperand);
+    //     lastOperand = 0;
+    //     operation = null;
+    //     inputWindow.value = result;
+    // }
+    
+})
+
+
+
--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,5 +1,5 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+let minValue = parseInt(prompt('Минимальное знание числа для игры от -999','0'));
+let maxValue = parseInt(prompt('Максимальное знание числа для игры до 999','100'));
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
@@ -12,12 +12,46 @@ orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
 document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
+    location.reload();
+    // minValue = 0;
+    // maxValue = 100;
+    // orderNumber = 0;
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseNumber = randomNamber(3);
+            // const answerPhrase = (phraseRandom === 1) ?
+            //     `Вы загадали неправильное число!\n\u{1F914}` :
+            //     `Я сдаюсь..\n\u{1F92F}`;
+            const answerPhrase;
+            switch (phraseNumber) {
+                case 1: 
+                    answerPhrase = `Ты загадал неправильное число!\n\u{1F914}`  ;
+                    break;
+                case 2:
+                    answerPhrase = `Что-то пошло не так\n\u{1F92F}`  ;
+                    break;
+                case 3:
+                    answerPhrase = `Где-то ты меня обманул \n\u{1F914}`  ;
+                    break;
+                
+            }
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            minValue = answerNumber  + 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
+})
+
+document.getElementById('btnLess').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
             const phraseRandom = Math.round( Math.random());
@@ -28,7 +62,7 @@ document.getElementById('btnOver').addEventListener('click', function () {
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            minValue = answerNumber  + 1;
+            maxValue = answerNumber  - 1;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
@@ -44,3 +78,8 @@ document.getElementById('btnEqual').addEventListener('click', function () {
     }
 })
 
+
+function randomNamber(count) {
+    let rndm = Math.round( Math.random() * count);
+    return rndm;
+}
